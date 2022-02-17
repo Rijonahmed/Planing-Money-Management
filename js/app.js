68903d2512904923b3f6f04af741560776131f63
Rijonahmed <rijon036@gmail.com>
2022-02-17 12:39:37 +0600

    fourth commit

--- js/app.js
+++ js/app.js
@@ -1,16 +1,73 @@
-document.getElementById('calculate-btn').addEventListener('click', function () {
-
-  // Income input value 
+function incomeAmount() {
   const incomeInput = document.getElementById('income-input');
   const incomeInputText = incomeInput.value;
   const incomeInputNumber = parseFloat(incomeInputText);
+  return incomeInputNumber;
+
+}
+
+function ExpensesFun() {
+  // Expenses input value
+  const foodInput = document.getElementById('food-input');
+  const foodInputText = foodInput.value;
+  const foodInputNumber = parseFloat(foodInputText);
 
 
+
+  const rentInput = document.getElementById('rent-input');
+  const rentInputText = rentInput.value;
+  const rentInputNumber = parseFloat(rentInputText);
+
+
+  const clothesInput = document.getElementById('clothes-input');
+  const clothesInputText = clothesInput.value;
+  const clothesInputNumber = parseFloat(clothesInputText);
+
+
+  // update total expenses
+  const totalExpenses = document.getElementById('total-expenses');
+  const priveousExpenses = totalExpenses.innerText;
+  const updateExpenses = parseFloat(priveousExpenses);
+  const updateTotalExpenses = updateExpenses + foodInputNumber + rentInputNumber + clothesInputNumber;
+  const Expenses = totalExpenses.innerText = updateTotalExpenses;
+  return Expenses;
+
+}
+function BalanceFun() {
+  const totalBalance = document.getElementById('total-balance');
+  const priveousBalance = totalBalance.innerText;
+  const updateBalance = parseFloat(priveousBalance);
+
+  totalBalance.innerText = updateBalance + incomeAmount() - ExpensesFun();
+
+  return totalBalance;
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById('calculate-btn').addEventListener('click', function () {
 
   // Expenses input value
   const foodInput = document.getElementById('food-input');
   const foodInputText = foodInput.value;
   const foodInputNumber = parseFloat(foodInputText);
+
 
 
   const rentInput = document.getElementById('rent-input');
@@ -34,14 +91,46 @@ document.getElementById('calculate-btn').addEventListener('click', function () {
   const priveousBalance = totalBalance.innerText;
   const updateBalance = parseFloat(priveousBalance);
 
-  totalBalance.innerText = updateBalance + incomeInputNumber - Expenses;
+  totalBalance.innerText = updateBalance + incomeAmount() - Expenses;
 
 
 
 
-  rentInput.value = '';
-  foodInput.value = '';
-  clothesInput.value = '';
+  // rentInput.value = '';
+  // foodInput.value = '';
+  // clothesInput.value = '';
+
+
+
+
+
+})
+document.getElementById('save-btn').addEventListener('click', function () {
+  const saveInput = document.getElementById('save-input');
+  const saveInputText = saveInput.value;
+  const saveInputNumber = parseFloat(saveInputText);
+
+  const savingAmount = document.getElementById('saving-amount');
+  const savingAmountText = savingAmount.innerText;
+  const savingAmountNumber = parseFloat(savingAmountText);
+
+  const remainingBalance = document.getElementById('remaining-balance');
+  const remainingBalanceText = remainingBalance.innerText;
+  const remainingBalanceNumber = parseFloat(remainingBalanceText);
+
+  // update parcent amount
+
+  const percentSavingAmount = incomeAmount() / 100;
+  const totalSavingAmount = percentSavingAmount * saveInputNumber;
+
+  savingAmount.innerText = totalSavingAmount;
+
+  const updateRemainingBalance = BalanceFun() - savingAmount.innerText;
+
+  remainingBalance.innerText = updateRemainingBalance;
+
+
+
 
 
 
